55cfad1223189f02730f019af658f714fd42ca93
Karapetyan Andres <key.andres@outlook.com>
2023-02-02 16:15:36 +0400

    last c# task

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int NaturSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    else if (m > n)
+        return 0;
+    return n + NaturSum(m, n - 1);
+}
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+int kk = Akkerman(m, n);
+Console.WriteLine(kk);
